62e339470d3023487c6de14f1b650b27e9036252
dnlm92 <dnlm92@me.com>
2015-07-04 01:16:52 -0500

    Hero Sprite with Jump Action.

--- Trex/GameScene.swift
+++ Trex/GameScene.swift
@@ -9,6 +9,7 @@
 import SpriteKit
 let background1 = SKSpriteNode(imageNamed: "bg1")
 let background2 = SKSpriteNode(imageNamed: "bg2")
+let hero = SKSpriteNode(imageNamed: "man")
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
@@ -22,27 +23,41 @@ class GameScene: SKScene {
         background2.position = CGPointMake(background1.size.width, 0)
         background2.zPosition = -15
         self.addChild(background2)
+
+        hero.anchorPoint = CGPointZero
+        hero.position = CGPointMake(70, 185)
+        hero.zPosition = 0
+        self.addChild(hero)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
+        let jumpUp = SKAction.moveToY(385, duration: 1)
+
+        hero.runAction(jumpUp, completion: { () -> Void in
+            let jumpDown = SKAction.moveToY(185, duration: 1)
+            hero.runAction(jumpDown)
+        })
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        self.moveBackground()
+    }
+
+    func moveBackground() {
         background1.position = CGPointMake(background1.position.x - 5, background1.position.y)
         background2.position = CGPointMake(background2.position.x - 5, background2.position.y)
-        
+
         if(background1.position.x < -background1.size.width)
         {
             background1.position = CGPointMake(background2.position.x + background2.size.width, background1.position.y)
         }
-        
+
         if(background2.position.x < -background2.size.width)
         {
             background2.position = CGPointMake(background1.position.x + background1.size.width, background2.position.y)
-            
+
         }
     }
 }
