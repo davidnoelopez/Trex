96bf04cd9b61d768557ac7d91201f96242c09993
Samuel Castillo <samuel.cas.96@gmail.com>
2015-07-04 00:33:09 -0500

    movimiento de background.

--- Trex/GameScene.swift
+++ Trex/GameScene.swift
@@ -7,11 +7,21 @@
 //
 
 import SpriteKit
+let background1 = SKSpriteNode(imageNamed: "bg1")
+let background2 = SKSpriteNode(imageNamed: "bg2")
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-       
+        background1.anchorPoint = CGPointZero
+        background1.position = CGPointMake(0, 0)
+        background1.zPosition = -15
+        self.addChild(background1)
+        
+        background2.anchorPoint = CGPointZero
+        background2.position = CGPointMake(0, background1.size.width)
+        background2.zPosition = -15
+        self.addChild(background2)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -21,5 +31,18 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        background1.position = CGPointMake(background1.position.x - 5, background1.position.y)
+        background2.position = CGPointMake(background2.position.x - 5, background2.position.y)
+        
+        if(background1.position.x < -background1.size.width)
+        {
+            background1.position = CGPointMake(background2.position.x + background2.size.width, background1.position.y)
+        }
+        
+        if(background2.position.y < -background2.size.width)
+        {
+            background2.position = CGPointMake(background1.position.x + background1.size.width, background2.position.y)
+            
+        }
     }
 }
